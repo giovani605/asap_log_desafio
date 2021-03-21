6a75e35b568078eed16f9e28d2a2d127dfda981c
Giovani Bossoni <giovanibossoni@hotmail.com>
2021-03-21 14:39:45 -0300

    primeiro commit

---
+++ src/main/java/com/example/asap_log_deasafio/AsapLogDeasafioApplication.java
@@ -0,0 +1,13 @@
+package com.example.asap_log_deasafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsapLogDeasafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsapLogDeasafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asap_log_deasafio/AsapLogDeasafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asap_log_deasafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsapLogDeasafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
